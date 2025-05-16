7414b4ea6bb5eef121ad51f31d57136161a1b33c
Anand Panda <anand612p@gmail.com>
2025-05-17 02:50:49 +0530

    add logo and favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BookOpen } from "lucide-react";
 
 const Header = () => {
@@ -7,7 +6,8 @@ const Header = () => {
             <div className="container mx-auto px-4 py-6">
                 <div className="flex flex-col md:flex-row items-center justify-between gap-4">
                     <div className="flex items-center gap-3">
-                        <BookOpen className="h-8 w-8 text-purple-400" />
+                        {/* <BookOpen className="h-8 w-8 text-purple-400" /> */}
+                        <img src="/icons/icon.png" alt="Logo" className="h-8 w-8" />
                         <h1 className="text-2xl md:text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-400">
                             Code फोड़
                         </h1>
