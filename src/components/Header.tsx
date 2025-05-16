e6f3098ee36df49994194b511cf962bb9fa257f7
Anand Panda <anand612p@gmail.com>
2025-05-17 02:54:25 +0530

    fix: update logo image path in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
                 <div className="flex flex-col md:flex-row items-center justify-between gap-4">
                     <div className="flex items-center gap-3">
                         {/* <BookOpen className="h-8 w-8 text-purple-400" /> */}
-                        <img src="/icons/icon.png" alt="Logo" className="h-8 w-8" />
+                        <img src="../../icons/icon.png" alt="Logo" className="h-8 w-8" />
                         <h1 className="text-2xl md:text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-400">
                             Code फोड़
                         </h1>
